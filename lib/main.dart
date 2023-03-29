1817aa6683830f6f9dc61493c79aa854a683eb4f
youne <younes72610@gmail.com>
2023-03-29 16:56:34 +0200

    Activité d'exercice

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projet_yw/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,20 +9,292 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  const MaterialApp(
       home: HomePage(),
     );
   }
 }
 
-class BeginnerPage extends StatelessWidget {
-  const BeginnerPage({Key? key}) : super(key: key);
+// Définition de l'interface utilisateur
+class BeginnerPage extends StatefulWidget {
+  List<Programme> programmes = initialiserProgrammesDebutant();
+
+  @override
+  BeginnerPageState createState() => BeginnerPageState();
+}
+
+class BeginnerPageState extends State<BeginnerPage> {
+  Programme? programmeSelectionne;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF79021D),
       appBar: AppBar(
-        title: Text("Programmes débutant"),
+        title: const Text("Programmes débutant",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontFamily: 'Kontora',
+        ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: ListView.builder(
+        itemCount: widget.programmes.length,
+        itemBuilder: (BuildContext context, int index) {
+          Programme programme = widget.programmes[index];
+          return ListTile(
+            title: Text(programme.titre,
+            style: const TextStyle(
+              color : Colors.white,
+              fontSize: 20,
+              fontFamily: 'Kontora',
+            ),
+            ),
+            onTap: (){
+              setState(() {
+                programmeSelectionne = programme;
+              });
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text(programme.titre),
+                    content: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          for(Exercice exercice in programme.exercices)
+                            ListTile(
+                              leading: Image.asset(exercice.icone),
+                              title: Text(exercice.nom),
+                              trailing: Ink(
+                                decoration: const ShapeDecoration(
+                                  color: Colors.black,
+                                  shape: CircleBorder(),
+                                ),
+                                padding: const EdgeInsets.all(10),
+                                child: Text('${exercice.repetitions}x',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Kontora',
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        child: const Text('Revenir à la liste'),
+                        onPressed: (){
+                          setState(() {
+                            programmeSelectionne = null;
+                          });
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text('Valider'),
+                        onPressed: (){
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder:(BuildContext context) =>
+                                  ProgrammePage(programme : programme),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Définition de l'interface utilisateur
+class IntermediatePage extends StatefulWidget {
+  List<Programme> programmes = initialiserProgrammesIntermediaire();
+
+  @override
+  IntermediatePageState createState() => IntermediatePageState();
+}
+
+class IntermediatePageState extends State<IntermediatePage>  {
+  Programme? programmeSelectionne;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF79021D),
+      appBar: AppBar(
+        title: const Text("Programmes Intermédiaire",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontFamily: 'Kontora',
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: ListView.builder(
+        itemCount: widget.programmes.length,
+        itemBuilder: (BuildContext context, int index) {
+          Programme programme = widget.programmes[index];
+          return ListTile(
+            title: Text(programme.titre),
+            onTap: (){
+              setState(() {
+                programmeSelectionne = programme;
+              });
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text(programme.titre),
+                    content: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          for(Exercice exercice in programme.exercices)
+                            ListTile(
+                              leading: Image.asset(exercice.icone),
+                              title: Text(exercice.nom),
+                              trailing: Text('${exercice.repetitions}x'),
+                            ),
+                        ],
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        child: const Text('Revenir à la liste'),
+                        onPressed: (){
+                          setState(() {
+                            programmeSelectionne = null;
+                          });
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text('Valider'),
+                        onPressed: (){
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder:(BuildContext context) =>
+                                  ProgrammePage(programme : programme),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Définition de l'interface utilisateur
+class ExpertPage extends StatefulWidget {
+  List<Programme> programmes = initialiserProgrammesExpert();
+
+  @override
+  ExpertPageState createState() => ExpertPageState();
+}
+
+class ExpertPageState extends State<ExpertPage> {
+  Programme? programmeSelectionne;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF79021D),
+      appBar: AppBar(
+        title: const Text("Programmes Expert",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontFamily: 'Kontora',
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: ListView.builder(
+        itemCount: widget.programmes.length,
+        itemBuilder: (BuildContext context, int index) {
+          Programme programme = widget.programmes[index];
+          return ListTile(
+            title: Text(programme.titre),
+            onTap: (){
+              setState(() {
+                programmeSelectionne = programme;
+              });
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text(programme.titre),
+                    content: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          for(Exercice exercice in programme.exercices)
+                            ListTile(
+                              leading: Image.asset(exercice.icone),
+                              title: Text(exercice.nom),
+                              trailing: Text('${exercice.repetitions}x'),
+                            ),
+                        ],
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        child: const Text('Revenir à la liste'),
+                        onPressed: (){
+                          setState(() {
+                            programmeSelectionne = null;
+                          });
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text('Valider'),
+                        onPressed: (){
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder:(BuildContext context) =>
+                                  ProgrammePage(programme : programme),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          );
+        },
       ),
     );
   }
@@ -36,61 +309,62 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF79021D),
       body:  Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/images/zoro.jpg"),
-              const Text("Roronoa Zoro",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontFamily: 'Kontora'
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const Text("Il faut se muscler les jambes pour devenir le meilleur",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontFamily: 'Kontora'
-                ),
-                textAlign: TextAlign.center,
-              ),
               ElevatedButton(
                 style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.amberAccent)
+                  backgroundColor: MaterialStatePropertyAll(Colors.black)
                 ),
-                  onPressed: () => print("click btn"),
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => BeginnerPage()),
+                    );
+                  },
                   child:const Text("Parcours débutant",
                   style: TextStyle(
                     fontSize: 20,
                     fontFamily: 'Kontora',
-                    color:Colors.black
+                    color:Colors.white
                   ),
                   ),
               ),
               ElevatedButton(
                 style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.amberAccent)
+                  backgroundColor: MaterialStatePropertyAll(Colors.black)
                 ),
-                onPressed: () => print("click btn"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => IntermediatePage()),
+                  );
+                },
                 child: const Text("Parcours intermédiaire",
                   style: TextStyle(
                     fontSize: 20,
                     fontFamily: 'Kontora',
-                    color:Colors.black
+                    color: Colors.white
                   ),
                 ),
               ),
               ElevatedButton(
                 style: const ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(Colors.amberAccent)
+                    backgroundColor: MaterialStatePropertyAll(Colors.black)
                 ),
-                onPressed: () => print("click btn"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ExpertPage()),
+                  );
+                },
                 child: const Text("Parcours expert",
                 style: TextStyle(
                   fontSize: 20,
                   fontFamily: 'Kontora',
-                  color:Colors.black
+                  color: Colors.white
                 ),
                 ),
               ),
