ce4d80b71e0a79bd78e359f2d60d0f5b743dca98
wass19 <setif7219@gmail.com>
2023-04-05 16:59:52 +0200

    push wassim update che pas 2.0

--- lib/programmeD/Chest_routine.dart
+++ lib/programmeD/Chest_routine.dart
@@ -2,9 +2,9 @@ import 'package:projet_yw/exercice.dart';
 import 'package:projet_yw/programme.dart';
 
 List<Exercice> exerciseList = [
-  Exercice(nom: 'Wide push ups', repetitions: 10, icone: "assets/images/pompes.png", video: "assets/video/pompes.gif"),
-  Exercice(nom: 'Decline push ups', repetitions: 8, icone: "assets/images/pompes.png", video: "assets/video/pompes.gif"),
-  Exercice(nom: 'Dips', repetitions: 5,icone: "assets/images/pompes.png", video: "assets/video/pompes.gif"),
+  Exercice(nom: 'pull ups', repetitions: 10, icone: "assets/images/traction.png", video: "assets/video/traction.gif"),
+  Exercice(nom: 'push ups', repetitions: 8, icone: "assets/images/pompes.png", video: "assets/video/pompes.gif"),
+  Exercice(nom: 'Dips', repetitions: 5,icone: "assets/images/dips.png", video: "assets/video/dips.gif"),
 ];
 
 Programme chest_routine = Programme(
